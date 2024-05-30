f72a304a84d824fa33e58e75a9be074657adbc9a
loic <loic.aviez91@gmail.com>
2024-05-30 15:08:49 +0200

    Updated database connection and session setup
    Updated db/database.py file to include comments in French regarding the SQLite database connection and session creation. These comments help in understanding the creation and connection process to the database.

--- db/database.py
+++ db/database.py
@@ -2,9 +2,12 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
+# Connexion à la base de données SQLite
 SQLALCHEMY_DATABASE_URL = "sqlite:///./items.db"
 
+# Création de la session
 engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False})
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
+# Création de la base de données
 Base = declarative_base()
