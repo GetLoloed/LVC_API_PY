f1f72e6486749007d4e7b7f34d356e1f5b90e19f
loic <loic.aviez91@gmail.com>
2024-05-31 10:09:40 +0200

    Add SSL support to server installation script
    The installation script has been updated to provide SSL support. It now includes the generation of private keys, a certificate signing request, and a self-signed certificate using OpenSSL. Additionally, the server IP was defined, and the uvicorn server now starts in HTTPS mode.

--- install.sh
+++ install.sh
@@ -6,7 +6,6 @@ sudo apt-get update -y && sudo apt-get upgrade -y
 # Installer python3, pip et venv
 sudo apt-get install -y python3 python3-pip python3-venv git
 
-
 # Créer un environnement virtuel
 python3 -m venv env
 
@@ -16,5 +15,17 @@ python3 -m venv env
 # Installer les dépendances
 pip install --isolated -r requirements.txt
 
-# Démarrer le serveur uvicorn
-nohup uvicorn main:app --host 0.0.0.0 --port 8000 &
+# Définir l'adresse IP du serveur
+SERVER_IP="10.17.10.7"
+
+# Générer une nouvelle clé privée avec OpenSSL
+openssl genrsa -out private.key 2048
+
+# Générer une demande de signature de certificat (CSR)
+openssl req -new -key private.key -out server.csr -subj "/CN=$SERVER_IP"
+
+# Générer un certificat SSL auto-signé
+openssl x509 -req -days 365 -in server.csr -signkey private.key -out certificate.crt
+
+# Démarrer le serveur uvicorn en HTTPS
+nohup uvicorn main:app --host $SERVER_IP --port 8000 --ssl-certfile certificate.crt --ssl-keyfile private.key &
